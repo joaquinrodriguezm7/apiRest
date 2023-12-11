1bc0b6c1a2ba9b889d62c688314d3845fbec3d89
joaquinrodriguezm7 <127951649+joaquinrodriguezm7@users.noreply.github.com>
2023-12-11 20:48:34 -0300

    arreglos

--- rest_vehiculo/views.py
+++ rest_vehiculo/views.py
@@ -7,10 +7,6 @@ from rest_framework.views import status
 from rest_framework.parsers import JSONParser
 from rest_framework.generics import get_object_or_404
 from django.http import JsonResponse
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from datetime import datetime
 # Create your views here.
 
 class UserView(APIView):
@@ -120,8 +116,7 @@ class ViajeView(APIView):
         try:
             data = JSONParser().parse(request)
             id_viaje = data['id_viaje']
-            nombre_usuario_cliente = data['nombre_usuario_cliente'] 
-            correo_pasajero = data['correo_pasajero'] 
+            nombre_usuario_cliente = data['nombre_usuario_cliente']  
             viaje = Viaje.objects.get(id_viaje=id_viaje)
 
             if viaje.capacidad_disponible > 0:
@@ -130,7 +125,6 @@ class ViajeView(APIView):
                 viaje.capacidad_disponible -= 1
                 viaje.save()
 
-                self.enviar_correo_confirmacion(nombre_usuario_cliente, correo_pasajero)
 
                 return JsonResponse({'mensaje': 'Has tomado el viaje correctamente'}, status=200)
             else:
@@ -140,33 +134,6 @@ class ViajeView(APIView):
         except Exception as e:
             print(f'Error en la vista: {repr(e)}')
             return JsonResponse(str(e), status=500, safe=False)
-
-    def enviar_correo_confirmacion(self, nombre_pasajero, correo_pasajero):
-        smtp_server = 'smtp.gmail.com'
-        smtp_port = 587
-        smtp_user = 'test1234.noreply@gmail.com'
-        smtp_password = 'contrasenna_'
-        usuario = Usuario.objects.get(nombre_usuario=nombre_pasajero)
-        usuario = usuario.nombre_usuario
-
-        subject = 'Viaje Tomado!'
-        body = f'El {usuario} ha tomado el viaje correctamente'
-
-        from_email = 'test1234.noreply@gmail.com'
-        to_email = correo_pasajero
-
-        message = MIMEMultipart()
-        message['From'] = from_email
-        message['To'] = to_email
-        message['Subject'] = subject
-        message.attach(MIMEText(body, 'plain'))
-
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.starttls()
-            server.login(smtp_user, smtp_password)
-
-            # Envío del mensaje
-            server.sendmail(from_email, to_email, message.as_string())
 
 class SedeView(APIView): 
     def get(self, request):
